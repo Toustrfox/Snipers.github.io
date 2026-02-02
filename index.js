5a9ddb2c813f182e0c232c9981cf46c5ddcc133d
Toustrfox <davidmego599@gmail.com>
2026-02-02 16:40:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("name").textContent = `Sniper | US Army Divison`;
+document.getElementById("made").textContent = `Made by Toustrfox`;
+document,getElementById("nabor").textContent = `Naborovy formular`;
